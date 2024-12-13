b26b63bbf2067c5b6264ee51f1207cd632cd01ad
NNON71 <110216423+NNON71@users.noreply.github.com>
2024-12-13 15:49:17 +0700

    Day1

---
+++ 2024/01/01.py
@@ -0,0 +1,13 @@
+txt = open("input.txt", "r").read()
+line = [list(map(int,line.split())) for line in txt.splitlines()]
+a1 = []
+a2 = []
+for a,b in line :
+    a1.append(a)
+    a2.append(b)
+a1.sort()
+a2.sort()
+for i in range(len(line)) :
+    print(a1[i], a2[i], end='\n')
+
+print(sum(list(abs(a1[i] - a2[i]) for i in range(len(line)))))    
