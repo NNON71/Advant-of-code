61e338787a979407c57b7ebc04261703d681e30d
NNON71 <110216423+NNON71@users.noreply.github.com>
2024-12-14 08:37:53 +0700

    opt

--- 2024/03/01.py
+++ 2024/03/01.py
@@ -1,10 +1,8 @@
 import re
 txt = open("input.txt", "r").read()
-x = re.findall(r"mul\(\d{1,3},\d{1,3}\)", txt)
-x = ''.join(x)
-x = re.findall(r"\d{1,3},\d{1,3}", x)
 ans = 0
-for i in x :
-    a, b = list(map(int, i.split(',')))
+for match in re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", txt):
+    a, b = map(int, match) 
     ans += a * b
+
 print(ans)
